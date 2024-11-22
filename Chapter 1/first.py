0037c34b6ae3fa3e350b8576f22b9674c8bec4b9
dev-arshdeepsingh <167911463+dev-arshdeepsingh@users.noreply.github.com>
2024-11-23 01:56:53 +0530

    Add files via upload

---
+++ Chapter 1/first.py
@@ -0,0 +1,6 @@
+print("Hello World")
+
+# Python is easy to understand
+# it is like learning english
+#Python is a software that commands our hardware
+# Python interpretter converts py script provided by us to machine code
---
+++ Chapter 1/module.py
@@ -0,0 +1,23 @@
+# Module is file of code written by someone, which can be reused by us
+
+
+import pyjokes   #But before this type "pip import pyjokes" in terminal
+                 #pip is used to install modules
+
+# print("Printing Jokes...")
+
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+
+# so thanks
+# that was my program
+# another line
+# Yet another line
+
+
+# We can make our terminal a calculator , by typing python & pressing enter first, then we can perform various operations like + - * / print etc in PCs CMD itself
+
+# Comments are used to make program readable for other programmers so that they can understand our code
+# we dont use multiline comments anymore as it is easy to select code and press ctrl+/ 
