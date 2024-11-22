412d972b6a3508d09ba0dc753fc2e8d67eb24f2d
dev-arshdeepsingh <167911463+dev-arshdeepsingh@users.noreply.github.com>
2024-11-23 02:10:03 +0530

    Add files via upload

---
+++ Chapter 1 - PS/Problem1.py
@@ -0,0 +1,55 @@
+print("""Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky.
+
+When the blazing sun is gone, When he nothing shines upon, Then you show your little light, Twinkle, twinkle, all the night.
+
+Then the traveler in the dark Thanks you for your tiny spark, He could not see which way to go, If you did not twinkle so.
+
+In the dark blue sky you keep, And often thro’ my curtains peep, For you never shut your eye 'Till the sun is in the sky.
+
+As your bright and tiny spark, Lights the traveler in the dark: Though I know not what you are, Twinkle, twinkle, little star.""")
+
+
+# Here we must triple double/single to print multiline as python recognises single quote upto 1 line only
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# *****************************SOLN****************************************
+
+
+# print(''' Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the trav'ller in the dark,
+# Thanks you for your tiny spark,
+# He could not see which way to go,
+# If you did not twinkle so.
+
+# In the dark blue sky you keep,
+# And often thro' my curtains peep,
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# 'Tis your bright and tiny spark,
+# Lights the trav'ller in the dark:
+# Tho' I know not what you are,
+# Twinkle, twinkle, little star.''')
---
+++ Chapter 1 - PS/Problem3.py
@@ -0,0 +1,6 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey I am good")
+engine.runAndWait()
+
+# first write this in terminal "pip install pyttsx3"
